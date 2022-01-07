842ad8a184a581673b1325b9b96eeeba80a09c50
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-07 12:10:31 +0100

    Update ControleurPrincipal.php
    create function structure

--- controleurs/ControleurPrincipal.php
+++ controleurs/ControleurPrincipal.php
@@ -5,6 +5,26 @@ class ControleurPrincipal
     private $_vue; // attribut « vue »
 
 
+     //Constructeur
+     public function __construct($vue) 
+     {
+        // $this->_vue=$vue;
+        $this->traiterDonneesGet();//appel de la fonction 1,
+        $this->afficher();//appel de la fonction 2;
+     }
+
+     //Fonction qui pour le moment ne fais rien
+     public function traiterDonneesGet()
+     {
+         
+     }
+
+      //Fonction qui affiche ok
+      public function afficher()
+      {
+          echo"OK"; //print ok
+      }
+ 
 
 }
 
