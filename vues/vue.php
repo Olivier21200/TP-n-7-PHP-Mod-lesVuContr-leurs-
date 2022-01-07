a17b6fdce55b8b45b8aeede67f8be0921f4983a9
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-07 15:07:37 +0100

    Update vue.php
    add main fonction structure

--- vues/vue.php
+++ vues/vue.php
@@ -5,12 +5,43 @@ class vue
     private $_titre; // attribut « titre »
 
 
-     //Constructeur
+     //Constructeur de la vue
      public function __construct($titre) 
      {
         $this->_titre = $titre; //init titre page
      }
- 
      
+     // fournira le code d’entête d’une page HTML jusqu’à la balise <body>
+     public function afficherEntete() 
+     {
+         
+     }
+
+     // fournira le code de pied d’une page HTML à partir de la balise </body> inclue
+     public function afficherPied() 
+     {
+
+     }
+
+     // affichera « Page introuvable
+     public function afficherCorps() 
+     {
+        echo"<p>Page introuvable</p>";
+     }
+     
+     //affichage de l'ensemble de la page
+     public function  afficher() 
+     {
+        $this->afficherEntete();
+        $this->afficherCorps();
+        $this->afficherPied();
+     }
+
+     //Fonction qui return le titre de la page
+     public function titre()
+     {
+         return $this->_titre;
+     }
+
 }
 ?>
