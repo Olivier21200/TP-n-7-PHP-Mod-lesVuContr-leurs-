c504b78b4f6c7193f1d501618209b20061f0b22b
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-07 12:17:44 +0100

    Create vue.php
    create structure vue

---
+++ vues/vue.php
@@ -0,0 +1,16 @@
+<?php 
+
+class vue
+{
+    private $_titre; // attribut « titre »
+
+
+     //Constructeur
+     public function __construct($titre) 
+     {
+        $this->_titre = $titre; //init titre page
+     }
+ 
+     
+}
+?>
